b801ddabd9b959221195b8a505193884798e89da
xiaobai2023123412412343 <128207903+xiaobai2023123412412343@users.noreply.github.com>
2023-06-19 10:30:12 +0800

    Update UpdateHandlers.cs

--- src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
+++ src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
@@ -1822,7 +1822,7 @@ if (trxBalance < 100)
     $"最后活跃：<b>{lastTransactionTime:yyyy-MM-dd HH:mm:ss}</b>\n" +
     $"————————<b>资源</b>————————\n"+
     $"用户标签：<b>{userLabel} {userLabelSuffix}</b>\n" +
-    $"交易笔数：<b>{transactions} （ ↑{transactionsOut} _ ↓{transactionsIn} ）</b>\n" +        
+    $"交易次数：<b>{transactions} （ ↑{transactionsOut} _ ↓{transactionsIn} ）</b>\n" +        
     $"USDT余额：<b>{usdtBalance.ToString("N2")} ≈ {cnyBalance.ToString("N2")}元人民币</b>\n" +
     $"TRX余额：<b>{trxBalance.ToString("N2")}  |  TRX能量不足，请{exchangeLink}</b>\n" +
     $"免费带宽：<b>{remainingBandwidth.ToString("N0")}/{totalBandwidth.ToString("N0")}</b>\n" +
@@ -1843,7 +1843,7 @@ else
     $"最后活跃：<b>{lastTransactionTime:yyyy-MM-dd HH:mm:ss}</b>\n" +
     $"————————<b>资源</b>————————\n"+
     $"用户标签：<b>{userLabel} {userLabelSuffix}</b>\n" +
-    $"交易笔数：<b>{transactions} （ ↑{transactionsOut} _ ↓{transactionsIn} ）</b>\n" +    
+    $"交易次数：<b>{transactions} （ ↑{transactionsOut} _ ↓{transactionsIn} ）</b>\n" +    
     $"USDT余额：<b>{usdtBalance.ToString("N2")} ≈ {cnyBalance.ToString("N2")}元人民币</b>\n" +
     $"TRX余额：<b>{trxBalance.ToString("N2")}  |  可供转账{availableTransferCount}次</b> \n" +
     $"免费带宽：<b>{remainingBandwidth.ToString("N0")}/{totalBandwidth.ToString("N0")}</b>\n" +
