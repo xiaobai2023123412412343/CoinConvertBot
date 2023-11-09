463ac9dcd4b55af61348a58d6c3a3718a0eccfdb
xiaobai2023123412412343 <128207903+xiaobai2023123412412343@users.noreply.github.com>
2023-11-09 17:34:57 +0800

    Update Program.cs

--- src/Telegram.CoinConvertBot/Program.cs
+++ src/Telegram.CoinConvertBot/Program.cs
@@ -112,7 +112,8 @@ static void ConfigureServices(HostBuilderContext Context, IServiceCollection Ser
         new BotCommand(){Command="cny",Description="合约助手"},
         new BotCommand(){Command="zijin",Description="资金费率"},   
         new BotCommand(){Command="faxian",Description="涨跌榜单"},    
-        new BotCommand(){Command="trc",Description="查询余额"},
+        new BotCommand(){Command="jkbtc",Description="涨跌榜单"},            
+        new BotCommand(){Command="trc",Description="币价监控"},
         new BotCommand(){Command="home",Description="个人中心"},            
         }).GetAwaiter().GetResult();
     }
