e43e08ccaab0321457ff7f02ad918b758ad718d1
xiaobai2023123412412343 <128207903+xiaobai2023123412412343@users.noreply.github.com>
2024-04-07 15:49:53 +0800

    Update UpdateHandlers.cs

--- src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
+++ src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
@@ -252,7 +252,7 @@ private static async Task CompareAndSendPriceChangeAsync(ITelegramBotClient botC
         return;
     }
 
-    // 获取15分钟前的价格数据，即队列中的第一条数据
+    // 获取30分钟前的价格数据，即队列中的第一条数据
     var fifteenMinutesAgoPrices = priceHistory.Peek();
 
     // 特别处理比特币和以太坊的价格变化
