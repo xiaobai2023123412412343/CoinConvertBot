9fb86ba50f80c5ae807919aa693c94edd2c3e3e8
xiaobai2023123412412343 <128207903+xiaobai2023123412412343@users.noreply.github.com>
2024-09-12 13:58:18 +0800

    Update UpdateHandlers.cs

--- src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
+++ src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
@@ -13976,7 +13976,7 @@ if (message.Type == MessageType.Text && message.Text.StartsWith("/jiankong"))
 if (messageText.Contains("费用") || messageText.Contains("能量") || messageText.Contains("/tron") || messageText.Contains("手续费") || messageText.Contains("能量租赁"))
 {
     // 向用户发送能量介绍
-    string multisigText = @"波场手续费说明（⚠️务必仔细阅读⚠️）
+    string multisigText = @$"波场手续费说明（⚠️务必仔细阅读⚠️）
 
 波场具有独特的资源模型，分为【带宽】和【能量】，每个账户初始具有 600 带宽 和 0 能量。
 转账USDT主要消耗能量，当账户可用能量不足时，燃烧TRX获取能量，燃烧的TRX就是我们常说的转账手续费。
@@ -13985,13 +13985,13 @@ if (messageText.Contains("费用") || messageText.Contains("能量") || messageT
 
 转账给有U的地址，消耗约 3.2万 能量；转账给没U的地址，消耗约 6.5万 能量。
 
-如果通过燃烧TRX获取3.2万能量，约需燃烧 14.00 TRX；
-如果通过燃烧TRX获取6.5万能量，约需燃烧 28.00 TRX。
+如果通过燃烧TRX获取3.2万能量，约需燃烧 {fixedCost} TRX；
+如果通过燃烧TRX获取6.5万能量，约需燃烧 {fixedCost * 2} TRX。
 
 通过提前租赁能量，可以避免燃烧TRX来获取能量，为您的转账节省大量TRX：
 
-租赁3.2万能量/日，仅需  7.00 TRX，节省   7.00 TRX (节省约50%)
-租赁6.5万能量/日，仅需13.00 TRX，节省 15.00 TRX (节省约55%)";
+租赁3.2万能量/日，仅需 {TransactionFee} TRX，节省 {savings} TRX (节省约{savingsPercentage}%)
+租赁6.5万能量/日，仅需{noUFee}TRX，节省{noUSavings} TRX (节省约{noUSavingsPercentage}%)";
 
     // 创建内联键盘按钮
     var inlineKeyboard = new InlineKeyboardMarkup(new[]
