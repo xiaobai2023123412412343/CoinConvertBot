3e75f04bdcc66e6990af383eacd35045587cbee3
xiaobai2023123412412343 <128207903+xiaobai2023123412412343@users.noreply.github.com>
2025-06-08 16:12:50 +0800

    Update UpdateHandlers.cs

--- src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
+++ src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
@@ -11802,7 +11802,8 @@ static async Task SendAdvertisement(ITelegramBotClient botClient, CancellationTo
                 "机器人收款地址:\n (<b>点击自动复制</b>):<code>TCL7X3bbPYAY8ppCgHWResGdR8pXc38Uu6</code>\n\n" +
                 "\U00002705 转U自动原地址返TRX,10U起兑!\n" +
                 "\U00002705 请勿使用交易所或汇旺钱包转账!\n" +
-                $"\U00002705 <u>购买能量套餐，单笔转账低至 {(int)TransactionFee}TRX！</u>\n" +
+                $"\U00002705 购买能量套餐，单笔转账低至 <b>{(int)TransactionFee}TRX</b>!\n" +
+                $"\U00002705 如需购买<b>ERC-20</b>手续费可联系管理员!\n" +		    
                 $"\U00002705 有任何问题,请私聊联系<a href=\"{channelLink}\">机器人管理员</a>\n\n" +
                 "<b>另代开TG高级会员</b>:\n\n" +
                 "\u2708三月高级会员：24.99 u\n" +
