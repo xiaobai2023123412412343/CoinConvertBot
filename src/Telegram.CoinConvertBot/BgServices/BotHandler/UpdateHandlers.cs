9e1345d3afd33ad9acc7cc7a37676313089da347
xiaobai2023123412412343 <128207903+xiaobai2023123412412343@users.noreply.github.com>
2025-06-12 01:03:52 +0800

    Update UpdateHandlers.cs

--- src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
+++ src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
@@ -11684,7 +11684,7 @@ for (int i = startIndex; i < endIndex; i++)
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },   
         new [] // 第二行
@@ -13327,7 +13327,7 @@ if (isNumberRange)
         if (!string.IsNullOrWhiteSpace(inputText))
         {
             // 修改正则表达式以匹配带小数点的数字计算
-            var containsKeywordsOrCommandsOrNumbersOrAtSign = Regex.IsMatch(inputText, @"^\/(start|yi|fan|qdgg|yccl|fu|btc|xamzhishu|xgzhishu|lamzhishu|qiand|shiwukxian|music|mairumaichu|charsi|provip|huiyuanku|zdcrsi|usd|more|usdt|tron|z0|cny|trc|home|jiankong|caifu|help|qunliaoziliao|baocunqunliao|bangdingdizhi|zijin|faxian|chaxun|xuni|ucard|jisuzhangdie|bijiacha|jkbtc)|更多功能|人民币|能量租赁|实时汇率|U兑TRX|合约助手|千百度查|地址监听|加密货币|外汇助手|监控|汇率|^[\d\+\-\*/\.\s]+$|^@");
+            var containsKeywordsOrCommandsOrNumbersOrAtSign = Regex.IsMatch(inputText, @"^\/(start|yi|fan|qdgg|yccl|fu|btc|xamzhishu|xgzhishu|lamzhishu|qiand|shiwukxian|music|mairumaichu|charsi|provip|huiyuanku|zdcrsi|usd|more|usdt|tron|z0|cny|trc|home|jiankong|caifu|help|qunliaoziliao|baocunqunliao|bangdingdizhi|zijin|faxian|chaxun|xuni|ucard|jisuzhangdie|bijiacha|jkbtc)|更多功能|人民币|能量租赁|实时汇率|U兑TRX|合约助手|询千百度|地址监听|加密货币|外汇助手|监控|汇率|^[\d\+\-\*/\.\s]+$|^@");
 
             // 检查输入文本是否为数字+货币的组合
             var isNumberCurrency = Regex.IsMatch(inputText, @"(^\d+\s*[A-Za-z\u4e00-\u9fa5]+$)|(^\d+(\.\d+)?(btc|比特币|eth|以太坊|usdt|泰达币|币安币|bnb|bgb|币记-BGB|okb|欧易-okb|ht|火币积分-HT|瑞波币|xrp|艾达币|ada|狗狗币|doge|shib|sol|莱特币|ltc|link|电报币|ton|比特现金|bch|以太经典|etc|uni|avax|门罗币|xmr)$)", RegexOptions.IgnoreCase);
@@ -14529,7 +14529,7 @@ const string BOT_USERNAME = "yifanfubot";//机器人用户名
 const long ADMIN_ID = 1427768220L;//指定管理员ID不转发
 
 // 存储机器人的所有命令
-string[] botCommands = { "/start", "/yi", "/fan", "/qdgg", "/yccl", "/fu", "/btc", "/usd", "/more","/music", "/cny","/lamzhishu","/xgzhishu","/xamzhishu", "/trc","/caifu","/qiand", "/usdt","/tron", "/home", "/jiankong", "/help", "/qunliaoziliao", "/baocunqunliao", "/bangdingdizhi", "/zijin", "/faxian", "/chaxun", "/xuni","/ucard","/bijiacha", "/jkbtc", "更多功能", "能量租赁", "实时汇率", "U兑TRX", "合约助手", "千百度查", "地址监听", "加密货币", "外汇助手","能量","energyComparison", "监控" };    
+string[] botCommands = { "/start", "/yi", "/fan", "/qdgg", "/yccl", "/fu", "/btc", "/usd", "/more","/music", "/cny","/lamzhishu","/xgzhishu","/xamzhishu", "/trc","/caifu","/qiand", "/usdt","/tron", "/home", "/jiankong", "/help", "/qunliaoziliao", "/baocunqunliao", "/bangdingdizhi", "/zijin", "/faxian", "/chaxun", "/xuni","/ucard","/bijiacha", "/jkbtc", "更多功能", "能量租赁", "实时汇率", "U兑TRX", "合约助手", "询千百度", "地址监听", "加密货币", "外汇助手","能量","energyComparison", "监控" };    
 
 if (message.Type == MessageType.Text)
 {	
@@ -16004,10 +16004,10 @@ if (messageText.StartsWith("z0") || messageText.StartsWith("/usdt")| messageText
             );
         });
 }
-// 检查是否是"千百度查"命令或 "/trc"
-if (message.Type == MessageType.Text && (message.Text.Equals("千百度查", StringComparison.OrdinalIgnoreCase) || message.Text.StartsWith("/trc")))
+// 检查是否是"询千百度"命令或 "/trc"
+if (message.Type == MessageType.Text && (message.Text.Equals("询千百度", StringComparison.OrdinalIgnoreCase) || message.Text.StartsWith("/trc")))
 {
-    string queryMessage = @$"你好呀~欢迎使用<b>千百度查</b>！
+    string queryMessage = @$"你好呀~欢迎使用<b>询千百度</b>！
 
 <b>支持区块链币种汇率、法币汇率、地址信息等查询！</b>
 
@@ -18275,7 +18275,7 @@ if (messageText.StartsWith("/jijianmoshi"))
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         }
     });
@@ -18298,7 +18298,7 @@ if (messageText.StartsWith("/wanzhengmoshi"))
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },
         new [] // 第二行
@@ -20693,7 +20693,7 @@ USDT余额： <b>{USDT}</b>
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },   
         new [] // 第二行
@@ -20835,7 +20835,7 @@ bool skipTRXMonitoring = parts.Any(part => part.Equals("TRX", StringComparison.O
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },   
         new [] // 第二行
@@ -20921,7 +20921,7 @@ catch (Telegram.Bot.Exceptions.ApiRequestException ex)
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },   
         new [] // 第二行
@@ -20988,7 +20988,7 @@ async Task<Message> UnBindAddress(ITelegramBotClient botClient, Message message)
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },   
         new [] // 第二行
@@ -21112,7 +21112,7 @@ async Task<Message> UnBindAddress(ITelegramBotClient botClient, Message message)
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },   
         new [] // 第二行
@@ -21230,7 +21230,7 @@ async Task<Message> PriceTRX(ITelegramBotClient botClient, Message message)
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },   
         new [] // 第二行
@@ -21356,7 +21356,7 @@ static async Task<Message> Start(ITelegramBotClient botClient, Message message)
             {
                 new KeyboardButton("U兑TRX"),
                 new KeyboardButton("实时汇率"),
-                new KeyboardButton("千百度查"),
+                new KeyboardButton("询千百度"),
                 new KeyboardButton("能量租赁"),
             },   
             new [] // 第二行
@@ -21468,7 +21468,7 @@ static async Task<Message> Start(ITelegramBotClient botClient, Message message)
             {
                 new KeyboardButton("U兑TRX"),
                 new KeyboardButton("实时汇率"),
-                new KeyboardButton("千百度查"),
+                new KeyboardButton("询千百度"),
                 new KeyboardButton("能量租赁"),
             },   
             new [] // 第二行
@@ -21548,7 +21548,7 @@ static async Task<Message> Start(ITelegramBotClient botClient, Message message)
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },   
         new [] // 第二行
@@ -21589,7 +21589,7 @@ static async Task<Message> zulin(ITelegramBotClient botClient, Message message)
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },   
         new [] // 第二行
@@ -21705,7 +21705,7 @@ static async Task<Message> ExecuteZjdhMethodAsync(ITelegramBotClient botClient, 
         {
             new KeyboardButton("U兑TRX"),
             new KeyboardButton("实时汇率"),
-            new KeyboardButton("千百度查"),
+            new KeyboardButton("询千百度"),
             new KeyboardButton("能量租赁"),
         },   
         new [] // 第二行
