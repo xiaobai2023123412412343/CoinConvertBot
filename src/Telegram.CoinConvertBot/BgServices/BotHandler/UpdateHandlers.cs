17098af44fe51ae43c6c028d604aa7270e94d60c
xiaobai2023123412412343 <128207903+xiaobai2023123412412343@users.noreply.github.com>
2025-08-30 20:01:31 +0800

    Update UpdateHandlers.cs

--- src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
+++ src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
@@ -15726,7 +15726,7 @@ else if(update.CallbackQuery.Data == "energyComparison")
     });
 
     // 尝试发送带图片的消息
-    const string imageUrl = "https://i.postimg.cc/zG7f5dmv/maxresdefault.jpg";
+    const string imageUrl = "https://i.postimg.cc/BQV0CdGx/1.jpg";
     try
     {
         await botClient.SendPhotoAsync(
@@ -17623,7 +17623,7 @@ try
         {
             await botClient.SendPhotoAsync(
                 chatId: message.Chat.Id,
-                photo: new InputOnlineFile("https://i.postimg.cc/zG7f5dmv/maxresdefault.jpg"),
+                photo: new InputOnlineFile("https://i.postimg.cc/BQV0CdGx/1.jpg"),
                 caption: multisigText,
                 parseMode: ParseMode.Html,
                 replyMarkup: inlineKeyboard
