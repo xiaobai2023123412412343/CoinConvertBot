0c1a63bf22d845ca34438aca63ba8b11c1344171
xiaobai2023123412412343 <128207903+xiaobai2023123412412343@users.noreply.github.com>
2025-09-04 16:11:15 +0800

    Update UpdateHandlers.cs

--- src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
+++ src/Telegram.CoinConvertBot/BgServices/BotHandler/UpdateHandlers.cs
@@ -22795,8 +22795,17 @@ if (messageText.StartsWith("预支"))
     string responseText = "请发送需要预支TRX的钱包地址查询是否满足要求：\n同时满足2点即可预支：\n⚠️仅限累计兑换 500 USDT 以上地址，\n⚠️地址余额大于 500 USDT且TRX余额低于13，\n⚠️预支的TRX能量仅够您向本机器人转账一次。\n\n如果查询满足条件，可<a href=\"" + adminLink + "\">联系管理员</a>直接预支TRX能量！";
     await botClient.SendTextMessageAsync(chatId: message.Chat.Id, text: responseText, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html, disableWebPagePreview: true);
 }  
+// 处理 /zjdh 命令
 if (messageText.StartsWith("/zjdh"))
 {
+    // 立即发送提示消息
+    await botClient.SendTextMessageAsync(
+        message.Chat.Id,
+        "正在查询，请稍后...",
+        parseMode: Telegram.Bot.Types.Enums.ParseMode.Html
+    );
+
+    // 调用查询方法
     var transferHistoryText = await TronscanHelper.GetTransferHistoryAsync();
     
     // 创建内联键盘按钮
@@ -22815,7 +22824,7 @@ if (messageText.StartsWith("/zjdh"))
         parseMode: Telegram.Bot.Types.Enums.ParseMode.Html,
         replyMarkup: inlineKeyboard
     );
-}  
+}
 // 如果用户发送的文本包含"多签"两个字
 if (messageText.Contains("多签") || messageText.Contains("多重签名"))
 {
